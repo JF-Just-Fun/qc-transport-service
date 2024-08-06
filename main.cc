44ce59a631b28b78bc98c87225b61459a1354faa
yinpo <347972265@qq.com>
2024-08-06 23:20:34 +0800

    fix: github action

--- main.cc
+++ main.cc
@@ -8,22 +8,8 @@ int main()
 {
   // Set HTTP listener address and port
   // drogon::app().addListener("0.0.0.0", PORT);
-  // Load config file
-  const char *env = std::getenv("BRANCH_NAME");
-  const char *envSuffix = "config.json";
-  if (env != nullptr)
-  {
-    if (std::strcmp(env, "test") == 0)
-    {
-      envSuffix = "config.test.json";
-    }
-    else if (std::strcmp(env, "main") == 0)
-    {
-      envSuffix = "config.main.json";
-    }
-  }
 
-  drogon::app().loadConfigFile(envSuffix);
+  drogon::app().loadConfigFile("config.json");
   // Run HTTP framework,the method will block in the internal event loop
   cout << "service starting at " << PORT << endl;
   drogon::app().enableSession(60 * 60 * 24).run();
